ddcf1679af0c1dc01fd9afc6a16310e3b2991325
afriweb1992 <ishitaafrin1992@gmail.com>
2024-05-05 20:38:04 +0600

    assignment

---
+++ js/script.js
@@ -0,0 +1,188 @@
+document.getElementById('btn1').addEventListener("click", () => {
+    const para = document.getElementById("para1")
+    let newPara = para.innerText
+    const numberPara = parseInt(newPara) + 1
+    para.innerText = numberPara
+    // price total 
+    // getting price 1
+    const price = document.getElementById("price1")
+    // taking price into innertext
+    let nwePrice = price.innerText
+    // converting new price into number
+    const numberPrice = parseInt(nwePrice)
+    // getting cost 
+    const cost = document.getElementById("cost")
+    // taking cost int inner Text
+    let newCost = cost.innerText
+    // converting new cost into number
+    const numberCost = parseInt(newCost)
+    // summission of cost and price
+    cost.innerText = numberPrice + numberCost
+    // getting total price
+    const total = document.getElementById("total")
+    // taking total price into inner text 
+    let newTotal = total.innerText
+    // converting new Total into number
+    const numberTotal = parseInt(newTotal)
+    // summission of numberTotal and numberCost
+    total.innerText = numberTotal + numberPrice
+
+})
+
+
+document.getElementById('btn2').addEventListener("click", () => {
+    const para = document.getElementById("para1")
+    let newPara = para.innerText
+    const numberPara = parseInt(newPara) + 1
+    para.innerText = numberPara
+    // price total 
+    // getting price 1
+    const price = document.getElementById("price2")
+    // taking price into innertext
+    let nwePrice = price.innerText
+    // converting new price into number
+    const numberPrice = parseInt(nwePrice)
+    // getting cost 
+    const cost = document.getElementById("cost")
+    // taking cost int inner Text
+    let newCost = cost.innerText
+    // converting new cost into number
+    const numberCost = parseInt(newCost)
+    // summission of cost and price
+    cost.innerText = numberPrice + numberCost
+    // getting total price
+    const total = document.getElementById("total")
+    // taking total price into inner text 
+    let newTotal = total.innerText
+    // converting new Total into number
+    const numberTotal = parseInt(newTotal)
+    // summission of numberTotal and numberCost
+    total.innerText = numberTotal + numberPrice
+
+})
+
+document.getElementById('btn3').addEventListener("click", () => {
+    const para = document.getElementById("para1")
+    let newPara = para.innerText
+    const numberPara = parseInt(newPara) + 1
+    para.innerText = numberPara
+    // price total 
+    // getting price 1
+    const price = document.getElementById("price3")
+    // taking price into innertext
+    let nwePrice = price.innerText
+    // converting new price into number
+    const numberPrice = parseInt(nwePrice)
+    // getting cost 
+    const cost = document.getElementById("cost")
+    // taking cost int inner Text
+    let newCost = cost.innerText
+    // converting new cost into number
+    const numberCost = parseInt(newCost)
+    // summission of cost and price
+    cost.innerText = numberPrice + numberCost
+    // getting total price
+    const total = document.getElementById("total")
+    // taking total price into inner text 
+    let newTotal = total.innerText
+    // converting new Total into number
+    const numberTotal = parseInt(newTotal)
+    // summission of numberTotal and numberCost
+    total.innerText = numberTotal + numberPrice
+
+})
+
+document.getElementById('btn4').addEventListener("click", () => {
+    const para = document.getElementById("para1")
+    let newPara = para.innerText
+    const numberPara = parseInt(newPara) + 1
+    para.innerText = numberPara
+    // price total 
+    // getting price 1
+    const price = document.getElementById("price4")
+    // taking price into innertext
+    let nwePrice = price.innerText
+    // converting new price into number
+    const numberPrice = parseInt(nwePrice)
+    // getting cost 
+    const cost = document.getElementById("cost")
+    // taking cost int inner Text
+    let newCost = cost.innerText
+    // converting new cost into number
+    const numberCost = parseInt(newCost)
+    // summission of cost and price
+    cost.innerText = numberPrice + numberCost
+    // getting total price
+    const total = document.getElementById("total")
+    // taking total price into inner text 
+    let newTotal = total.innerText
+    // converting new Total into number
+    const numberTotal = parseInt(newTotal)
+    // summission of numberTotal and numberCost
+    total.innerText = numberTotal + numberPrice
+
+})
+document.getElementById('btn5').addEventListener("click", () => {
+    const para = document.getElementById("para1")
+    let newPara = para.innerText
+    const numberPara = parseInt(newPara) + 1
+    para.innerText = numberPara
+    // price total 
+    // getting price 1
+    const price = document.getElementById("price5")
+    // taking price into innertext
+    let nwePrice = price.innerText
+    // converting new price into number
+    const numberPrice = parseInt(nwePrice)
+    // getting cost 
+    const cost = document.getElementById("cost")
+    // taking cost int inner Text
+    let newCost = cost.innerText
+    // converting new cost into number
+    const numberCost = parseInt(newCost)
+    // summission of cost and price
+    cost.innerText = numberPrice + numberCost
+    // getting total price
+    const total = document.getElementById("total")
+    // taking total price into inner text 
+    let newTotal = total.innerText
+    // converting new Total into number
+    const numberTotal = parseInt(newTotal)
+    // summission of numberTotal and numberCost
+    total.innerText = numberTotal + numberPrice
+
+})
+document.getElementById('btn6').addEventListener("click", () => {
+    const para = document.getElementById("para1")
+    let newPara = para.innerText
+    const numberPara = parseInt(newPara) + 1
+    para.innerText = numberPara
+    // price total 
+    // getting price 1
+    const price = document.getElementById("price6")
+    // taking price into innertext
+    let nwePrice = price.innerText
+    // converting new price into number
+    const numberPrice = parseInt(nwePrice)
+    // getting cost 
+    const cost = document.getElementById("cost")
+    // taking cost int inner Text
+    let newCost = cost.innerText
+    // converting new cost into number
+    const numberCost = parseInt(newCost)
+    // summission of cost and price
+    cost.innerText = numberPrice + numberCost
+    // getting total price
+    const total = document.getElementById("total")
+    // taking total price into inner text 
+    let newTotal = total.innerText
+    // converting new Total into number
+    const numberTotal = parseInt(newTotal)
+    // summission of numberTotal and numberCost
+    total.innerText = numberTotal + numberPrice
+
+})
+
+
+
+
